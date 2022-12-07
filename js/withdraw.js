9ab2fabf3c4b74eeafa207c6e5195123042981ca
Dewan Mohammod Zahid Hasan <dewanmohammodzahidhasan@gmail.com>
2022-12-07 18:53:12 +0600

    first commit

--- js/withdraw.js
+++ js/withdraw.js
@@ -11,9 +11,10 @@
 
 document.getElementById("btn-withdraw").addEventListener("click", function () {
   const newWithdrawAmount = getInputFieldValueById("withdraw-field");
-  const previousWithdrawTotal = getTextElementValueById("withdraw-total ");
+  const previousWithdrawTotal = getTextElementValueById("withdraw-total");
   const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
-  setTextElementValueById(withdraw - total, newWithdrawTotal);
+  console.log(newWithdrawTotal);
+  setTextElementValueById("withdraw-total", newWithdrawTotal);
   const previousBalanceTotal = getTextElementValueById("balance-total");
   const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
   setTextElementValueById("balance-total", newBalanceTotal);
